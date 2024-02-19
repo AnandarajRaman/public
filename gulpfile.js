33f53d158e4990db4102ac55fca08a43f783b507
AnandarajRaman <156906376+AnandarajRaman@users.noreply.github.com>
2024-02-19 16:58:27 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -58,13 +58,13 @@ const spellings = [
 ];
 
 
-gulp.task('ship-to-private', function (done) {
+gulp.task('sync-to-private', function (done) {
 
     shelljs.exec(`git config --global user.email "${user_mail}"`);
     shelljs.exec(`git config --global user.name "${user}"`);
     
     // Check for changes in the docs folder
-    var changes = shelljs.exec(`git diff --name-only HEAD^ HEAD docs/`);
+    var changes = shelljs.exec(`git diff --name-only docs/`);
     var changedFileNames = changes.stdout.split('\n').filter(Boolean); // Filter out any empty strings
 
     // Check if there are any changes in the docs folder
